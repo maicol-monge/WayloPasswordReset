58dd8a45a05954e8a608872d1ecbca10e481eef9
Maicol Monge <maicol.monge@catolica.edu.sv>
2025-11-21 13:12:28 -0600

    A ver

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 3000 }
+  server: { port: 3000 },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  },
+  publicDir: 'public'
 })
